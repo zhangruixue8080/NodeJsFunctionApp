97125d65bcf3cd0128b993bc2c8f116c064daf6b
Zhang, Ruixue <Ruixue.Zhang@emc.com>
2019-04-22 11:27:24 +0800

    test

--- AddedFunction1/index.js
+++ AddedFunction1/index.js
@@ -1,7 +1,6 @@
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-
     if (req.query.name || (req.body && req.body.name)) {
         context.res = {
             // status: 200, /* Defaults to 200 */
